059d802ef672a010c0a33f6f4805409bd1253505
Lucas <friendlybeatle@gmail.com>
2024-10-24 23:57:20 -0700

    lab6 changes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,16 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - JS for Lab 6 !
+// Author: Lucas Larkin
+// Date: October 24, 2024
+
+// Corbin Wilhelm <ljwilhel@ucsc.edu>
+// Lucas Larkin <djlarkin@ucsc.edu>
+// October 2024
+// License: Public Domain
 
 // Constants
-
+const myTransport =  ['The Bus', 'My Beautiful and Beloved Orange Honda Civic']
 // Functions
+var myBeautifulAndBelovedOrangeHondaCivic = {
+  make: "Honda",
+  model: "Civic",
+  color "Orange",
+  year: 1974,
+  age: function() {
+    return 2024 - this.year;
+  }
+}
+document.writeIn("My two main methods of transport are: myTransport[0]" + "and" + "myTransport[1]" + "</br>");
+document.writeIn("Want to know more about my beautiful and beloved orange Honda Civic? Look no further than right here!: <pre>", 
+  JSON.stringify(myBeautifulAndBelovedOrangeHondaCivic, null, '\t'), "</pre>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
 }
+
 
 function main() {
   console.log("Main function started.");
